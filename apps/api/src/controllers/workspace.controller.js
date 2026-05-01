c157336357d851bae63275f245ad6475f97875ca
Hlamong55 <bijoymarma55@gmail.com>
2026-05-01 23:13:53 +0600

    feat: add workspace member and role system

--- apps/api/src/controllers/workspace.controller.js
+++ apps/api/src/controllers/workspace.controller.js
@@ -1,5 +1,6 @@
 const prisma = require("../utils/prisma");
 
+// Create Workspace
 exports.createWorkspace = async (req, res) => {
   const { name } = req.body;
 
@@ -16,4 +17,86 @@ exports.createWorkspace = async (req, res) => {
   });
 
   res.json(workspace);
+};
+
+// Get My Workspaces
+exports.getMyWorkspaces = async (req, res) => {
+  const workspaces = await prisma.workspaceMember.findMany({
+    where: {
+      userId: req.user.id,
+    },
+    include: {
+      workspace: true,
+    },
+  });
+
+  res.json(workspaces);
+};
+
+// Add Member
+exports.addMember = async (req, res) => {
+  try {
+    const { workspaceId } = req.params;
+    const { email, role = "MEMBER" } = req.body;
+
+    // Check admin
+    const me = await prisma.workspaceMember.findFirst({
+      where: { workspaceId, userId: req.user.id },
+    });
+
+    if (!me || me.role !== "ADMIN") {
+      return res.status(403).json({ message: "Only admin can add members" });
+    }
+
+    // Find user
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Prevent duplicate
+    const exists = await prisma.workspaceMember.findFirst({
+      where: { workspaceId, userId: user.id },
+    });
+
+    if (exists) {
+      return res.status(400).json({ message: "Already a member" });
+    }
+
+    // Create member
+    const member = await prisma.workspaceMember.create({
+      data: {
+        workspaceId,
+        userId: user.id,
+        role,
+      },
+    });
+
+    res.json(member);
+  } catch (err) {
+    res.status(500).json({ message: "Add member failed" });
+  }
+};
+
+//  Get Members
+exports.getMembers = async (req, res) => {
+  const { workspaceId } = req.params;
+
+  const members = await prisma.workspaceMember.findMany({
+    where: { workspaceId },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+        },
+      },
+    },
+  });
+
+  res.json(members);
 };
--- apps/api/src/routes/workspace.routes.js
+++ apps/api/src/routes/workspace.routes.js
@@ -1,9 +1,23 @@
 const router = require("express").Router();
 const auth = require("../middleware/auth.middleware");
+
 const {
   createWorkspace,
+  getMyWorkspaces,
+  addMember,
+  getMembers,
 } = require("../controllers/workspace.controller");
 
+// Create workspace
 router.post("/", auth, createWorkspace);
+
+// Get my workspaces
+router.get("/", auth, getMyWorkspaces);
+
+// Add member
+router.post("/:workspaceId/members", auth, addMember);
+
+// Get members
+router.get("/:workspaceId/members", auth, getMembers);
 
 module.exports = router;
